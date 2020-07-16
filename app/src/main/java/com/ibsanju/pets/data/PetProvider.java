7ec3853b3b1737586a9d0f43bc8d1fef9a57b0ec
IbsanjU <bharath2668@gmail.com>
2020-07-16 18:05:07 +0530

    Uri matcher

--- app/src/main/java/com/ibsanju/pets/data/PetProvider.java
+++ app/src/main/java/com/ibsanju/pets/data/PetProvider.java
@@ -1,6 +1,7 @@
 package com.ibsanju.pets.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -8,6 +9,27 @@ import android.net.Uri;
  * {@link ContentProvider} for Pets app.
  */
 public class PetProvider extends ContentProvider {
+    /** URI matcher code for the content URI for the pets table */
+    private static final int PETS = 100;
+
+    /** URI matcher code for the content URI for a single pet in the pets table */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+
+        // TODO: Add 2 content URIs to URI matcher
+    }
 
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
