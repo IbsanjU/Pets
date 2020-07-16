cfe9fcf4d94da6399b629f23375d13f51aa3c8ac
IbsanjU <bharath2668@gmail.com>
2020-07-16 19:11:35 +0530

    remaining validation

--- app/src/main/java/com/ibsanju/pets/data/PetProvider.java
+++ app/src/main/java/com/ibsanju/pets/data/PetProvider.java
@@ -1,4 +1,5 @@
 package com.ibsanju.pets.data;
+
 import android.content.ContentProvider;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -13,13 +14,20 @@ import com.ibsanju.pets.data.PetContract.PetEntry;
 /**
  * {@link ContentProvider} for Pets app.
  */
-public class PetProvider extends ContentProvider {
-    /** URI matcher code for the content URI for the pets table */
+public
+class PetProvider extends ContentProvider {
+    /**
+     * Tag for the log messages
+     */
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+    /**
+     * URI matcher code for the content URI for the pets table
+     */
     private static final int PETS = 100;
-
-    /** URI matcher code for the content URI for a single pet in the pets table */
+    /**
+     * URI matcher code for the content URI for a single pet in the pets table
+     */
     private static final int PET_ID = 101;
-
     /**
      * UriMatcher object to match a content URI to a corresponding code.
      * The input passed into the constructor represents the code to return for the root URI.
@@ -36,16 +44,17 @@ public class PetProvider extends ContentProvider {
         // TODO: Add 2 content URIs to URI matcher
     }
 
-    /** Tag for the log messages */
-    public static final String LOG_TAG = PetProvider.class.getSimpleName();
-
-    /** Database helper Object */
+    /**
+     * Database helper Object
+     */
     private PetDbHelper mDbHelper;
+
     /**
      * Initialize the provider and the database helper object.
      */
     @Override
-    public boolean onCreate() {
+    public
+    boolean onCreate() {
         // TODO: Create and initialize a PetDbHelper object to gain access to the pets database.
         // Make sure the variable is a global variable, so it can be referenced from other
         // ContentProvider methods.
@@ -57,8 +66,9 @@ public class PetProvider extends ContentProvider {
      * Perform the query for the given URI. Use the given projection, selection, selection arguments, and sort order.
      */
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
-                        String sortOrder) {
+    public
+    Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                 String sortOrder) {
         // Get readable database
         SQLiteDatabase database = mDbHelper.getReadableDatabase();
 
@@ -84,7 +94,7 @@ public class PetProvider extends ContentProvider {
                 // arguments that will fill in the "?". Since we have 1 question mark in the
                 // selection, we have 1 String in the selection arguments' String array.
                 selection = PetEntry._ID + "=?";
-                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
 
                 // This will perform a query on the pets table where the _id equals 3 to return a
                 // Cursor containing that row of the table.
@@ -101,7 +111,8 @@ public class PetProvider extends ContentProvider {
      * Insert new data into the provider with the given ContentValues.
      */
     @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
+    public
+    Uri insert(Uri uri, ContentValues contentValues) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case PETS:
@@ -110,23 +121,30 @@ public class PetProvider extends ContentProvider {
                 throw new IllegalArgumentException("Insertion is not supported for " + uri);
         }
     }
+
     /**
      * Insert a pet into the database with the given content values. Return the new content URI
      * for that specific row in the database.
      */
-    private Uri insertPet(Uri uri, ContentValues values) {
+    private
+    Uri insertPet(Uri uri, ContentValues values) {
         // Check that the name is not null
         String name = values.getAsString(PetEntry.COLUMN_PET_NAME);
         if (name == null) {
             throw new IllegalArgumentException("Pet requires a name");
         }
-
         // TODO: Finish sanity checking the rest of the attributes in ContentValues
+        
         Integer gender = values.getAsInteger(PetEntry.COLUMN_PET_GENDER);
         if (gender == null || !PetEntry.isValidGender(gender)) {
             throw new IllegalArgumentException("Pet requires valid gender");
         }
 
+        // If the weight is provided, check that it's greater than or equal to 0 kg
+        Integer weight = values.getAsInteger(PetEntry.COLUMN_PET_WEIGHT);
+        if (weight != null && weight < 0) {
+            throw new IllegalArgumentException("Pet requires valid weight");
+        }
 
         // Get writeable database
         SQLiteDatabase database = mDbHelper.getWritableDatabase();
@@ -147,7 +165,8 @@ public class PetProvider extends ContentProvider {
      * Updates the data at the given selection and selection arguments, with the new ContentValues.
      */
     @Override
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+    public
+    int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
         return 0;
     }
 
@@ -155,7 +174,8 @@ public class PetProvider extends ContentProvider {
      * Delete the data at the given selection and selection arguments.
      */
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
+    public
+    int delete(Uri uri, String selection, String[] selectionArgs) {
         return 0;
     }
 
@@ -163,7 +183,8 @@ public class PetProvider extends ContentProvider {
      * Returns the MIME type of data for the content URI.
      */
     @Override
-    public String getType(Uri uri) {
+    public
+    String getType(Uri uri) {
         return null;
     }
 
