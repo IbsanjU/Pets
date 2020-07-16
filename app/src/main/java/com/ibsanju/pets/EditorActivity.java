dc5ce609490494323bf9df15bc503a4dfae1079c
IbsanjU <bharath2668@gmail.com>
2020-07-17 04:56:10 +0530

    Prevent empty field crash and Validating empty feilds on click save

--- app/src/main/java/com/ibsanju/pets/EditorActivity.java
+++ app/src/main/java/com/ibsanju/pets/EditorActivity.java
@@ -188,7 +188,16 @@ public class EditorActivity extends AppCompatActivity implements
         String nameString = mNameEditText.getText().toString().trim();
         String breedString = mBreedEditText.getText().toString().trim();
         String weightString = mWeightEditText.getText().toString().trim();
-        int weight = Integer.parseInt(weightString);
+
+        // Check if this is supposed to be a new pet
+        // and check if all the fields in the editor are blank
+        if (mCurrentPetUri == null && TextUtils.isEmpty(nameString)
+                && TextUtils.isEmpty(breedString) && TextUtils.isEmpty(weightString)
+                && mGender == PetEntry.GENDER_UNKNOWN) {
+            // Since no fields were modified, we can return early without creating a new pet.
+            // No need to create ContentValues and no need to do any ContentProvider operations.
+            return;
+        }
 
         // Create a ContentValues object where column names are the keys,
         // and pet attributes from the editor are the values.
@@ -196,12 +205,19 @@ public class EditorActivity extends AppCompatActivity implements
         values.put(PetEntry.COLUMN_PET_NAME, nameString);
         values.put(PetEntry.COLUMN_PET_BREED, breedString);
         values.put(PetEntry.COLUMN_PET_GENDER, mGender);
+
+        /* If the weight is not provided by the user, don't try to parse the string into an
+         integer value. Use 0 by default.*/
+        int weight = 0;
+        if (!TextUtils.isEmpty(weightString)) {
+            weight = Integer.parseInt(weightString);
+        }
         values.put(PetEntry.COLUMN_PET_WEIGHT, weight);
 
-// Determine if this is a new or existing pet by checking if mCurrentPetUri is null or not
+        /* Determine if this is a new or existing pet by checking if mCurrentPetUri is null or not*/
         if (mCurrentPetUri == null) {
-            // This is a NEW pet, so insert a new pet into the provider,
-            // returning the content URI for the new pet.
+            /*This is a NEW pet, so insert a new pet into the provider,
+             returning the content URI for the new pet. */
             Uri newUri = getContentResolver().insert(PetEntry.CONTENT_URI, values);
 
             // Show a toast message depending on whether or not the insertion was successful.
