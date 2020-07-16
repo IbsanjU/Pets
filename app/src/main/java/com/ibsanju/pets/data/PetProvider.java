aad3f5d5cd7aac5261f0c6af8be7d48de85d524d
IbsanjU <bharath2668@gmail.com>
2020-07-16 18:40:17 +0530

    update pet provider insert() method

--- app/src/main/java/com/ibsanju/pets/data/PetProvider.java
+++ app/src/main/java/com/ibsanju/pets/data/PetProvider.java
@@ -6,6 +6,7 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+import android.util.Log;
 
 import com.ibsanju.pets.data.PetContract.PetEntry;
 
@@ -101,7 +102,38 @@ public class PetProvider extends ContentProvider {
      */
     @Override
     public Uri insert(Uri uri, ContentValues contentValues) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return insertPet(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+    /**
+     * Insert a pet into the database with the given content values. Return the new content URI
+     * for that specific row in the database.
+     */
+    private Uri insertPet(Uri uri, ContentValues values) {
+
+        // TODO: Insert a new pet into the pets database table with the given ContentValues
+
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Insert the new pet with the given values
+        long id = database.insert(PetEntry.TABLE_NAME, null, values);
+        // If the ID is -1, then the insertion failed. Log an error and return null.
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        // Return the new URI with the ID (of the newly inserted row) appended at the end
+        return ContentUris.withAppendedId(uri, id);
     }
 
     /**
@@ -127,4 +159,7 @@ public class PetProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
+
+
 }
+
